85fa16ca1223fbf7c05caf562ffb684fd6d679fe
Abbas-k <46700767+Abbas-k@users.noreply.github.com>
2019-09-17 08:29:04 +0430

    Create sum.java

---
+++ sum/sum.java
@@ -0,0 +1,10 @@
+public Class Sum
+{
+  public static void main(String[] args)
+  {
+    int firstNumber = 5;
+    int secondNumber = 10;
+    int sum = firstNumber + secondNumber;
+    System.out.println("Sum = " + sum);
+  }
+}
